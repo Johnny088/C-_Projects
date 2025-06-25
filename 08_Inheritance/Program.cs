e40554a9b4497409710f69d3e8a7ab20faf73aeb
Johnny <esp25goodlife@gmail.com>
2025-06-25 17:50:05 +0200

    first lesson

---
+++ 08_Inheritance/Program.cs
@@ -0,0 +1,112 @@
+﻿
+namespace _08_Inheritance
+{
+
+    abstract class GeometricShape
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public int H { get; set; }
+
+        public abstract void GetArea();
+        public abstract void GetPerimeter();
+
+
+    }
+    class Triangle : GeometricShape
+    {
+        public override void GetArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void GetPerimeter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Square : GeometricShape
+    {
+        public override void GetArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void GetPerimeter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Rhombus : GeometricShape
+    {
+        public override void GetArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void GetPerimeter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Rectangle : GeometricShape
+    {
+        public override void GetArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void GetPerimeter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Parallelogram : GeometricShape
+    {
+        public override void GetArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void GetPerimeter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Trapezoid : GeometricShape
+    {
+        public override void GetArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void GetPerimeter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Circle : GeometricShape
+    {
+        public override void GetArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void GetPerimeter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
